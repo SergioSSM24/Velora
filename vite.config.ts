542ced4bcf830503573cdbc5161719427b66df54
SergioSSM24 <115508366+SergioSSM24@users.noreply.github.com>
2025-09-26 12:42:38 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/github-pages/',
+    base: process.env.VITE_BASE_PATH || "/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
